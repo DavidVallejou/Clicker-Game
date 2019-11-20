36fb995dd9c523ad525a0da1ffc36125b42abd61
David Vallejo <davidvallejou@gmail.com>
2019-11-20 16:16:32 -0500

    Click Game

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var PORT = process.env.PORT|| 3000;
 
 // if (process.env.NODE_ENV === "production") {
-    app.use(express.static("public"));
+    app.use(express.static("build"));
 //   }
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
